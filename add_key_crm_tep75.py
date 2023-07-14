b3a11e7696c4998d6ff37b23fa85484c71dd62dc
Arkasha - infinix <severe90000@gmail.com>
2023-07-14 14:12:13 +0300

    Добавил возможность добавлять ключи в 2 дома

--- add_key_crm_tep75.py
+++ add_key_crm_tep75.py
@@ -10,19 +10,28 @@ mac_tep75_gates_3 = "08:13:CD:00:02:30"
 mac_tep75_entrance_1 = "08:13:CD:00:07:96"
 mac_tep75_entrance_2 = "08:13:CD:00:07:85"
 
+mac_tep75_1_entrance_1 = "08:13:CD:00:04:12"
+mac_tep75_1_entrance_2 = "08:13:CD:00:07:77"
+
+list_home = """Выберете дом (введите номер строки):
+1) Тепличная 75
+2) Тепличная 75/1"""
+
 mac = str()
 key = str()
 file = open('keys.txt', 'r')
 apartment = str()
 num_entrance = str() # Номер подезда
 
+num_home = int()
+
 url = f"https://crm.dtel.ru/front/device-keys/{mac}/create-key"
 Cookie = "PHPSESSID_TD_CRM=65r54f5l3r6ne8hpvsmfh27js3; _csrf=11319b03dd44ab191f63862cf53eb348ea36d7e773bb47d99df5f539ce7169e8a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22pN_pI8mSdIOpNKkEcMUyRxD2VbNMfWPH%22%3B%7D; _identity=f236f7e09617f4beaf7878357688d69d7be7640c15935b049ca5fb38014c009da%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A17%3A%22%5B20%2Cnull%2C2592000%5D%22%3B%7D"
 
 #----ввод-данных-----------------------------------------------
 
-# print("Введите номер подезад: ", end = "") # выводи сообщение без новой строки 
-# num_entrance = input()
+print(list_home) # выводи сообщение без новой строки 
+num_home = int(input())
 
 print("Введите номер квартиры: ", end = "") # выводи сообщение без новой строки 
 apartment = input()
@@ -114,7 +123,7 @@ def entrance_1_75():
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    if response: print(f"OK - ключ добавлен - подезд 1 - {key}")
+    if response: print(f"OK - ключ добавлен - Дом 75 - подезд 1 - {key}")
     else: print(response.status_code);print(response.text)
 
 def entrance_2_75():
@@ -135,22 +144,75 @@ def entrance_2_75():
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    if response: print(f"OK - ключ добавлен - подезд - 2 - {key}", end = "")
+    if response: print(f"OK - ключ добавлен - Дом 75 - подезд - 2 - {key}")
     else: print(response.status_code);print(response.text)
+
+def entrance_1_75_1():
+    
+    mac = mac_tep75_1_entrance_1
+    url = f"https://crm.dtel.ru/front/device-keys/{mac}/create-key"
+
+    payload = json.dumps({
+    "value": key,
+    "numberSystem": "16",
+    "flatNum": apartment,
+    "inner": "1"
+    })
+    headers = {
+    'Cookie': Cookie,
+    'Content-Type': 'application/json',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    if response: print(f"OK - ключ добавлен - Дом 75/1 - подезд 1 - {key}")
+    else: print(response.status_code);print(response.text)
+
+def entrance_2_75_2():
+    
+    mac = mac_tep75_1_entrance_2
+    url = f"https://crm.dtel.ru/front/device-keys/{mac}/create-key"
+
+    payload = json.dumps({
+    "value": key,
+    "numberSystem": "16",
+    "flatNum": apartment,
+    "inner": "1"
+    })
+    headers = {
+    'Cookie': Cookie,
+    'Content-Type': 'application/json',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    if response: print(f"OK - ключ добавлен - Дом 75/1 - подезд 2 - {key}")
+    else: print(response.status_code);print(response.text)
+
 
 for key in file:
 
-    gates_1()
-    gates_2()
-    gates_3()
+    # gates_1()
+    # gates_2()
+    # gates_3()
 
     apartment = int(apartment)
 
-    if 1 <= apartment <= 96: # 1 - 96 квартир
-        entrance_1_75()
+    if num_home == 1:
 
-    elif 97 <= apartment <= 272: # 97 - 272 квартиры
-        entrance_2_75()
+        if 1 <= apartment <= 96: # 1 - 96 квартир
+            entrance_1_75()
+
+        elif 97 <= apartment <= 272: # 97 - 272 квартиры
+            entrance_2_75()
+
+    elif num_home == 2:
+
+        if 1 <= apartment <= 96: # 1 - 96 квартир
+            entrance_1_75_1()
+
+        elif 97 <= apartment <= 272: # 97 - 272 квартиры
+            entrance_2_75_2()
 
     else: print("Введите корректные данные")
 
